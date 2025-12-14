2f0ca0aaaef83e0a3ef87b530fbe79280463707c
Caps Unlock <capsonluck@gmail.com>
2025-12-14 22:39:57 +0100

    task_5

--- task_4.sql
+++ task_4.sql
@@ -1,9 +1,4 @@
--- SQL script to print the full description of the BOOKS table 
--- in the alx_book_store database without using DESCRIBE or EXPLAIN.
---
-
--- 1. Explicitly select the database
 USE alx_book_store;
 
--- 2. Print the full description of the BOOKS table
-SHOW FULL COLUMNS FROM BOOKS;
+
+SHOW COLUMNS FROM BOOKS;
