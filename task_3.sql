a9be62614860892228e9c1b074bccfbbd507d2ff
Caps Unlock <capsonluck@gmail.com>
2025-12-14 22:08:01 +0100

    task_3.sql

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+--
+-- SQL script to list all tables in the currently selected database.
+-- The database name is expected to be provided when executing the script (e.g., mysql -u user -p alx_book_store < task_3.sql)
+--
+SHOW TABLES;
